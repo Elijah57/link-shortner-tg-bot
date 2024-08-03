9e0cbfb3935746ff3f6882338a42cf7131dff1d8
Elijah Echekwu <elijahechekwu30@gmail.com>
2024-08-03 17:32:19 +0100

    feat: Added static file for testing the service and geolocation tracking with ip address

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, redirect, jsonify
+from flask import Flask, request, redirect, jsonify, render_template, url_for
 from dotenv import load_dotenv
 from db import connect_redis
+from ip_config import ip_info_init, get_geo_data
+import datetime
 import random
 import string
 import os
@@ -13,25 +15,39 @@ charset = string.ascii_letters + string.digits
 
 app = Flask(__name__)
 
+
 redis_client = connect_redis()
 
+ip_handler = ip_info_init()
 
 
 def generate_key():
     return ''.join(random.choices(charset, k=key_length))
 
-@app.route('/shorten', methods=['POST'])
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('/shorten', methods=["GET",'POST'])
 def shorten_url():
-    long_url = request.json['url']
+    long_url = request.form['url']
     while True:
         key = generate_key()
         if not redis_client.exists(key):
             redis_client.set(key, long_url)
-            return jsonify({'short_url': f'{base_link}/{key}'})
+            return render_template("index.html", short_url= f'{base_link}/{key}')
 
 @app.route('/<key>')
 def redirect_url(key):
     long_url = redis_client.get(key)
+
+    ip_address = request.remote_addr
+    user_agent = request.headers.get("User-Agent")
+    device = "mobile" if "Mobi" in user_agent else "desktop"
+    timestamp = datetime.datetime.now()
+    geo_location = get_geo_data(ip_handler, ip_address)
+
+    print(ip_address, user_agent, device, timestamp, geo_location)
     if long_url:
         return redirect(long_url.decode("utf-8"))
     return 'URL not found', 404
